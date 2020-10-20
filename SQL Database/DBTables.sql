d285675f6737613d77d74d11be8adf8dc1722421
Will <willtcrumpton@gmail.com>
2020-10-20 07:26:41 -0700

    Tables design altered

--- SQL Database/DBTables.sql
+++ SQL Database/DBTables.sql
@@ -1,9 +1,14 @@
 CREATE TABLE containers (
     c_ID INT,
+    c_batchID INT,
     c_dateTime DATETIME,
-    c_latitude DOUBLE(20, 15),
-    c_longitude DOUBLE(20, 15),
     c_row INT,
     c_section INT,
     c_notes VARCHAR(255)
+)
+
+CREATE TABLE batch (
+    b_ID INT,
+    c_latitude DOUBLE(20, 15),
+    c_longitude DOUBLE(20, 15)
 );
