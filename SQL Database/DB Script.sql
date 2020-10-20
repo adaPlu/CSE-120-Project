222303b05639af7f27a346dabe159ac832105ad8
apluguez <apluguez@ucmerced.edu>
2020-10-20 09:38:26 -0700

    some data

--- SQL Database/DB Script.sql
+++ SQL Database/DB Script.sql
@@ -1,13 +1,25 @@
 /*insert, delete, sort, search*/
 INSERT INTO containers (c_ID,c_batchID,c_dateTime,c_row,c_section,c_notes)
-VALUES  (55, 80, '1996-01-15', 7, 10, "Some notes");
+VALUES  (8, 1, '1996-01-15', 7, 10, "Some notes");
 INSERT INTO containers (c_ID,c_batchID,c_dateTime,c_row,c_section,c_notes)
-VALUES  (56, 80, '1996-05-01', 4, 10, "Some notes");
+VALUES  (1, 1, '1996-05-01', 4, 10, "Some notes");
 INSERT INTO containers (c_ID,c_batchID,c_dateTime,c_row,c_section,c_notes)
-VALUES  (57, 85, '1996-03-15', 3, 2, "Some notes");
+VALUES  (2, 1, '1996-03-15', 3, 2, "Some notes");
 INSERT INTO containers (c_ID,c_batchID,c_dateTime,c_row,c_section,c_notes)
-VALUES  (58, 85, '1996-02-01', 8, 4, "Some notes");
+VALUES  (3, 1, '1996-02-01', 8, 4, "Some notes");
+INSERT INTO containers (c_ID,c_batchID,c_dateTime,c_row,c_section,c_notes)
+VALUES  (4, 2, '1996-01-15', 7, 10, "Some notes");
+INSERT INTO containers (c_ID,c_batchID,c_dateTime,c_row,c_section,c_notes)
+VALUES  (5, 2, '1996-05-01', 4, 10, "Some notes");
+INSERT INTO containers (c_ID,c_batchID,c_dateTime,c_row,c_section,c_notes)
+VALUES  (6, 3, '1996-03-15', 3, 2, "Some notes");
+INSERT INTO containers (c_ID,c_batchID,c_dateTime,c_row,c_section,c_notes)
+VALUES  (7, 4, '1996-02-01', 8, 4, "Some notes");
 INSERT INTO batch (b_batchID,b_latitude,b_longitude)
-VALUES  (80, 6.021, 5.312);
+VALUES  (1, 6.021, 5.312);
 INSERT INTO batch (b_batchID,b_latitude,b_longitude)
-VALUES  (85, 8.021, 3.312);
+VALUES  (2, 8.021, 3.312);
+INSERT INTO batch (b_batchID,b_latitude,b_longitude)
+VALUES  (3, 4.021, 1.312);
+INSERT INTO batch (b_batchID,b_latitude,b_longitude)
+VALUES  (4, 3.021, 2.312);
