7d0d514c8535617bd873825f10064df28229bb4c
adaplu <adapluguez@gmail.com>
2020-11-05 11:14:38 -0800

    Icon Changes.

--- MorningStarApp/app/src/androidTest/java/com/morningstar/barcodevisionapi/ExampleInstrumentedTest.java
+++ MorningStarApp/app/src/androidTest/java/com/morningstar/barcodevisionapi/ExampleInstrumentedTest.java
@@ -17,10 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {// throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
 
-        assertEquals("com.journaldev.barcodevisionapi", appContext.getPackageName());
+        assertEquals("com.morningstar.barcodevisionapi", appContext.getPackageName());
     }
 }
