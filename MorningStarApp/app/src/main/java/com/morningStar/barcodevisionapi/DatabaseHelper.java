d55b166e7f6f09af90de21ea061d694d73282192
Will <willtcrumpton@gmail.com>
2020-11-13 23:43:17 -0800

    DatabaseHelper and DBManager classes added. MainActivity modified to initialize DB.

---
+++ MorningStarApp/app/src/main/java/com/morningStar/barcodevisionapi/DatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.morningStar.barcodevisionapi;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // Database Information
+    static final String DB_NAME = "MorningStar.DB";
+
+    // database version
+    static final int DB_VERSION = 1;
+
+    // Creating table query
+    private static final String CREATE_CONTAINER_TABLE = "create table Container(" +
+            "c_BatchID INTEGER," +
+            "c_Barcode TEXT," +
+            "c_Date TEXT," +
+            "c_Row INTEGER," +
+            "c_Section INTEGER);";
+
+    private static final String CREATE_BATCH_TABLE = "create table Batch(" +
+            "b_BatchID INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "b_Latitude REAL," +
+            "b_Longitude REAL);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_CONTAINER_TABLE);
+        db.execSQL(CREATE_BATCH_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS Container");
+        db.execSQL("DROP TABLE IF EXISTS Batch");
+        onCreate(db);
+    }
+}
--- MorningStarApp/app/src/main/java/com/morningStar/barcodevisionapi/MainActivity.java
+++ MorningStarApp/app/src/main/java/com/morningStar/barcodevisionapi/MainActivity.java
@@ -1,19 +1,33 @@
 package com.morningstar.barcodevisionapi;
 
+import android.app.Activity;
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
+import android.database.Cursor;
+//import android.support.v7.app.AppCompactActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.morningStar.barcodevisionapi.DBManager;
+
+public class MainActivity extends Activity implements View.OnClickListener {
 
     Button btnTakePicture, btnScanBarcode;
+
+    private DBManager dbManager;
+    private SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbManager = new DBManager(this);
+        dbManager.open();
+        Cursor container_cursor = dbManager.fetch_containers();
+        Cursor batch_cursor = dbManager.fetch_batches();
+
         initViews();
     }
 
@@ -22,11 +36,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnScanBarcode = findViewById(R.id.btnScanBarcode);
         btnTakePicture.setOnClickListener(this);
         btnScanBarcode.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        /*
+
         switch (v.getId()) {
             case R.id.btnTakePicture:
                 startActivity(new Intent(MainActivity.this, PictureBarcodeActivity.class));
@@ -35,12 +50,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
                 break;
         }
-        */
-        if(v.getId() == R.id.btnTakePicture){
-            startActivity(new Intent(MainActivity.this, PictureBarcodeActivity.class));
-        }
-        if(v.getId() == R.id.btnScanBarcode){
-            startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
-        }
+
     }
 }
