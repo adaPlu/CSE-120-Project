a7c052504523930b114500d8e710eec4364f1fb8
adaplu <adapluguez@gmail.com>
2020-11-18 14:37:53 -0800

    Attempt to connect batch management button to batch management screen.

---
+++ MorningStarApp/app/src/main/java/com/morningstar/barcodevisionapi/BatchManagementActivity.java
@@ -0,0 +1,18 @@
+package com.morningstar.barcodevisionapi;
+
+import android.content.Intent;
+import android.database.Cursor;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
+
+public class BatchManagementActivity extends AppCompatActivity  implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
